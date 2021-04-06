536f2e4ea14d880bd8824a20ada65b31336950d4
KalleManuel <74299603+KalleManuel@users.noreply.github.com>
2021-04-06 18:14:22 +0200

    difying code needed

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -5,17 +5,29 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    // ThePoints = Create an array of possible points to earn (24 in total): 100, 200, 300, 400 and so on until 2400
+    // DistrubutePoints() = Scramble these numbers and distrubute them in a new array (numPlaceHolders) with an determined order from 1 - 24
+    // A number can't occour more then once in the new array
+    // Thegame should have an active score that changes every hour starting with numPlaceHoldes[0] and ending with numPlaceHoldes[23] 
+    // Every new day (at 00.00) Scramblethe numbers again
+
 
     public int[] thePoints;
     public int[] numPlaceHolders;
-    public int theScore;
-    public int tries;
     public int gameLenght = 24;
 
-    public int days;
-    public bool gameOn;
+    // if the player pushes the button they will gain the score assigned to that hour.
+    // The score gaind shall be added to a total score.
+    // the player can only push the button twice each hour
+    // The player can only puch the button ten times a day
+    // Theplayer shall be able to see they score in a UI.
 
     public Button theButton;
+    public int theScore;
+    public bool canPushButton;
+    public int tries;
+
+
 
     // Start is called before the first frame update
     void Start()
